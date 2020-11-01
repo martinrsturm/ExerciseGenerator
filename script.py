ab6a31921e44408051a89a3acef45da7e7a0d7da
Martin Sturm <martinrsturm@gmail.com>
2020-11-01 16:27:30 +0100

    Changed name of "main" script

--- script.py
+++
@@ -1,9 +0,0 @@
-import exergen
-from tkinter import *
-
-fractionExpr = exergen.FractionProblemGenerator(['3', '9', '9', '0'])
-print(fractionExpr.generate_problem_and_solution())
-
-root = Tk()
-app = exergen.MainWindow(root)
-root.mainloop()
